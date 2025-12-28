c2313f38e28f21c40eb041894dcc1128d4cf05fa
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-12-28 13:08:01 -0300

    Update estoque.py

--- src/farmacia/estoque.py
+++ src/farmacia/estoque.py
@@ -1,10 +1,6 @@
-from .produto import Produto
-from core.funcionario import Funcionario
-
 class Estoque:
     def __init__(self):
         self.__produtos = {}  
 
     def get_produtos(self):
         return self.__produtos
-
