6555ee49052580a9d3fba1acbd8c185c23ef1deb
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-12-29 20:41:29 -0300

    Update test_estoque.py

--- tests/test_estoque.py
+++ tests/test_estoque.py
@@ -1,21 +1,23 @@
 # teste de implementacoes
-
 from src.core.funcionario import Funcionario
 from src.farmacia.estoque import Estoque
 from src.farmacia.produto import Produto
-
-estoque = Estoque()
-
-tadala = Produto('p1', 'tadala', 10.0, 'cimed')
-nebacetin = Produto('p2', 'nebacetin', 8.5, 'cimed')
-
-funcionario = Funcionario('João', '12345678900', '1990-01-01')
-
-funcionario.registrar_produto(estoque, tadala, 5)
-funcionario.registrar_produto(estoque, nebacetin, 3)
-
-print("Estoque inicial:", funcionario.consultar_estoque(estoque))
-
-funcionario.registrar_venda(estoque, tadala, 2)
-
-print("Estoque após venda:", funcionario.consultar_estoque(estoque))
+estoque = Estoque() 
+funcionario = Funcionario('João', '12345678900', '1990-01-01','1200',1)
+p1 = Produto(1,'Dipirona-25mg-Comprimido',2.30,'Cimed')
+p2 = Produto(2,'Dorflex-5mg-Comprimido',1.90,'Sanofi')
+print(50*'-')
+print('INFORMAÇÕES GERAIS DO FUNCIONARIO')
+print(f'NOME: {funcionario.nome}')
+print(f'CPF: {funcionario.get_cpf()}')
+print(f'NASCIMENTO: {funcionario.get_data_nascimento()}')
+print(f'SALÁRIO: {funcionario.get_salario_base()}')
+print(f'ID: {funcionario.get_id()}')
+print(50*'-')
+print('TESTANDO METODOS DE FUNCIONARIO')
+funcionario.adicionar_produto(p1,12,estoque)
+funcionario.adicionar_produto(p2,24,estoque)
+print(f'Estoque Antes da venda: {funcionario.consultar(estoque)}')
+funcionario.vender_produto(1,2,estoque)
+funcionario.vender_produto(2,4,estoque)
+print(f'Estoque Depois da venda: {funcionario.consultar(estoque)}')
