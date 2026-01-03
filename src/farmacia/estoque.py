7011599f1bd53e48f975a48e8985d187b2b7b85b
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-01-03 14:41:18 -0300

    Update estoque.py

--- src/farmacia/estoque.py
+++ src/farmacia/estoque.py
@@ -1,7 +1,27 @@
 class Estoque:
     def __init__(self):
-        self.__produtos = {} # possivel mudanca, ao inves de um dicionario, uma lista com tuplas, ex.: [(produto1, 123), (produto2, 189)]
+        self.__produtos = {} 
 
     def get_produtos(self):
         return self.__produtos
     
+    def adicionar_produto(self, produto, quantidade):
+        produtos_estoque = self.__produtos
+        if produto.getId() in produtos_estoque:
+            produtos_estoque[produto.getId()]["quantidade"] += quantidade
+        else:
+            produtos_estoque[produto.getId()] = {
+                "produto": produto,
+                "quantidade": quantidade
+            }
+    def remover_produto(self, id_produto, quantidade):
+        produtos_estoque = self.__produtos
+        if id_produto in produtos_estoque:
+            if produtos_estoque[id_produto]["quantidade"] >= quantidade:
+                produtos_estoque[id_produto]["quantidade"] -= quantidade
+                return True
+            else:
+                print('Quantidade insuficiente')
+        else:
+            print('Produto não encontrado.')
+        return False
