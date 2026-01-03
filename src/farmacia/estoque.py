6b3cb3773600ed5cf67a264bb8be6642b74c37fc
unknown <nicolas.raony06@gmail.com>
2026-01-03 15:54:31 -0300

    Update: estrutura de produtos e metodo adicionar

--- src/farmacia/estoque.py
+++ src/farmacia/estoque.py
@@ -1,20 +1,25 @@
 class Estoque:
     def __init__(self):
-        self.__produtos = {} 
+        self.__produtos = {} # estrutura: {obj_produto: qtd}
 
     def get_produtos(self):
         return self.__produtos
     
-    def adicionar_produto(self, produto, quantidade):
-        produtos_estoque = self.__produtos
-        if produto.getId() in produtos_estoque:
-            produtos_estoque[produto.getId()]["quantidade"] += quantidade
-        else:
-            produtos_estoque[produto.getId()] = {
-                "produto": produto,
-                "quantidade": quantidade
-            }
-    def remover_produto(self, id_produto, quantidade):
+    def adicionar_produto(self, produto, quantidade: int):
+        from src.farmacia.produto import Produto
+        if not isinstance(produto, Produto): # isso aqui serve pra evitar problemas de importacao circular
+            raise ValueError("Parametro produto deve ser uma instacia do tipo Produto")
+        
+        for produto_estoque in self.__produtos.keys():
+            if (produto == produto_estoque) or (produto.getId() == produto_estoque.getId()):
+                raise ValueError("Produto já existe em estoque") # aqui verifica ser a referencia ao objeto de produto já tá em estoque.
+            
+        if quantidade < 0:
+            raise ValueError("Quantidade deve ser maior que 0") # essa validação pode ser tirada caso estoque possa ter um produto sem disponibilidade de compra.
+            
+        self.__produtos.update({produto: quantidade}) # adiciona uma nova chave e valor em dicionario de produtos
+
+    def remover_produto(self, id_produto, quantidade): # não mudei nada aqui, mas deve precisar de reajuste
         produtos_estoque = self.__produtos
         if id_produto in produtos_estoque:
             if produtos_estoque[id_produto]["quantidade"] >= quantidade:
