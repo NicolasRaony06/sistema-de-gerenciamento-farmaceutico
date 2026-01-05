7eed5d3b558fcef56b9efb2a46399a672e81f8aa
unknown <nicolas.raony06@gmail.com>
2026-01-04 23:36:32 -0300

    Add: gerador id

---
+++ src/utils/gerador_id.py
@@ -0,0 +1,13 @@
+from random import randint
+
+def getIdProduto(obj, maxIds = 1000):
+    '''Recebe um objeto que tenha o atributo de classe 'allIds' do tipo list implementado. Parametro de geracoes de Id, 'maxIds', por padrao esta em 1000. Retorna um Id unico e erro caso o numero maximo de Id seja alcancado.'''
+    id = randint(1, maxIds)
+    counter = 0
+    while id in obj.allIds:
+        if counter > len(obj.allIds):
+            raise ValueError("Numero maximo de ID alcancado")
+        id = randint(1, maxIds)
+        counter += 1
+    obj.allIds.append(id)
+    return id
