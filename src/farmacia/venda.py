56630e6905e1f3168a11dfe55b688709d9a0bd3d
unknown <nicolas.raony06@gmail.com>
2026-01-06 20:13:29 -0300

    Add: metodo subtotal | Update: setPrecoTotal

--- src/farmacia/venda.py
+++ src/farmacia/venda.py
@@ -1,7 +1,7 @@
 #implementacao de classe Venda
 from src.core.funcionario import Funcionario
 from datetime import datetime
-from decimal import Decimal
+from decimal import Decimal, ROUND_DOWN
 from src.utils.gerador_id import getIdProduto
 
 class Venda:
@@ -23,7 +23,7 @@ class Venda:
         return self.__funcionario
     
     def getPrecoTotal(self):
-        '''Retorna preco total da Venda'''
+        '''Retorna preço total da Venda'''
         return self.__precoTotal
     
     def getProdutos(self):
@@ -31,26 +31,43 @@ class Venda:
         return self.__produtos
     
     def adicionarProduto(self, produto, quantidade : int):
-        '''Adiciona produto em Venda, caso produto ja exista, a sua quantidade e somada. Recebe como parametro um objeto do tipo Produto e uma quantidade inteira.'''
+        '''Adiciona produto em Venda, caso produto já exista, a sua quantidade é somada. Recebe como parâmetro um objeto do tipo Produto e uma quantidade inteira. Não é possível adicionar produto se venda tiver sido finalizada'''
         from src.farmacia.produto import Produto
         if not isinstance(produto, Produto):
-            raise ValueError('Metodo deve receber um objeto do tipo Produto')
+            raise ValueError('Método deve receber um objeto do tipo Produto')
+        
+        if self.__precoTotal:
+            raise PermissionError('Venda já finalizada. Não é mais possível adicionar produtos')
         
         if quantidade < 0:
             raise ValueError('Quantidade deve ser maior que 0')
         
-        for index, vendaProduto in enumerate(self.__produtos):
-            if produto.__repr__() in vendaProduto:
-                self.__produtos[index] = (produto, vendaProduto[1] + quantidade)
+        for index, itemVenda in enumerate(self.__produtos):
+            if produto.__repr__() in itemVenda:
+                self.__produtos[index] = (produto.__repr__(), itemVenda[1] + quantidade)
                 return True
                 
         self.__produtos.append((produto.__repr__(), quantidade))
+        
+    def setPrecoTotal(self, funcionario : Funcionario):
+        '''Altera preco total da venda com base em produtos já adicionados e suas quantidades. Recebe um objeto do tipo Funcionario. Esse metodo sinaliza a finalizacao da compra'''
+        if self.__precoTotal:
+            raise PermissionError('Venda já foi finalizada.')
+        
+        self.__precoTotal = self.__subTotal()
+        log = f'Data:{datetime.now()};Funcionario:{Funcionario};Preco:{self.__precoTotal}'
+        self.__logAlteracoes.append(log)
+    
+    def __subTotal(self):
+        '''Método privado para calcular subtotal da venda.'''
+        from src.farmacia.produto import Produto
+        subTotal = Decimal(0)
+        for itemVenda in self.__produtos:
+            try:
+                produto = eval(itemVenda[0])
+            except Exception as e:
+                raise TypeError(f"Erro ao instanciar Produto: {e}")
 
-    def setPrecoTotal(self, funcionario: Funcionario, valor: Decimal):
-        '''Setter para alterar preco total da venda. Recebe um objeto do tipo Funcionario e um valor do tipo Decimal. Adiciona a atual modificacao ao Log de Alteracoes'''
-        if valor > 0:
-            self.__precoTotal = valor
-            log = f'Data:{datetime.now()};Funcionario:{funcionario};Valor:{valor}'
-            self.__logAlteracoes.append(log)
+            subTotal += produto.getPreco() * itemVenda[1]
 
-        raise ValueError("O valor de preço total deve ser maior que 0")
+        return subTotal
