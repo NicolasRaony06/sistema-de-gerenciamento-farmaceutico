a3283983b2b77d7a14f17c7772bad94573548694
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-01-05 23:22:22 -0300

    Update test_estoque.py

--- tests/test_estoque.py
+++ tests/test_estoque.py
@@ -1,24 +1,26 @@
 # teste de implementacoes
 # python -m tests.test_estoque
-from src.core.funcionario import Funcionario
+from src.utils.gerador_id import getIdProduto
+from src.core.gerente import Gerente
 from src.farmacia.estoque import Estoque
 from src.farmacia.produto import Produto
 estoque = Estoque() 
-funcionario = Funcionario('João', '12345678900', '1990-01-01','1200',1)
-p1 = Produto(1,'Dipirona-25mg-Comprimido',2.30,'Cimed')
-p2 = Produto(2,'Dorflex-5mg-Comprimido',1.90,'Sanofi')
+g = Gerente('João', '12345678900', '1990-01-01','1200',1)
+p1 = Produto('Dipirona-25mg-Comprimido',2.30,'Cimed')
+p2 = Produto('Dorflex-5mg-Comprimido',1.90,'Sanofi')
 print(50*'-')
 print('INFORMAÇÕES GERAIS DO FUNCIONARIO')
-print(f'NOME: {funcionario.nome}')
-print(f'CPF: {funcionario.get_cpf()}')
-print(f'NASCIMENTO: {funcionario.get_data_nascimento()}')
-print(f'SALÁRIO: {funcionario.get_salario_base()}')
-print(f'ID: {funcionario.get_id()}')
+print(f'NOME: {g.nome}')
+print(f'CPF: {g.get_cpf()}')
+print(f'NASCIMENTO: {g.get_data_nascimento()}')
+print(f'SALÁRIO: {g.get_salario_base()}')
+print(f'ID: {g.get_id()}')
 print(50*'-')
 print('TESTANDO METODOS DE FUNCIONARIO')
-funcionario.adicionar_produto(p1,12,estoque)
-funcionario.adicionar_produto(p2,24,estoque)
-print(f'Estoque Antes da venda: {funcionario.consultar(estoque)}')
-funcionario.vender_produto(1,2,estoque)
-funcionario.vender_produto(2,4,estoque)
-print(f'Estoque Depois da venda: {funcionario.consultar(estoque)}')
+g.adicionar_produto_estoque(p1,12,estoque)
+g.adicionar_produto_estoque(p2,24,estoque)
+print(f'Estoque Antes da venda: {g.consultar(estoque)}')
+g.vender_produto(p1.getId(),2,estoque)
+g.vender_produto(p2.getId(),4,estoque)
+print(estoque.consultar_quantidade_por_id(p1.getId()))
+print(f'Estoque Depois da venda: {g.consultar(estoque)}')
