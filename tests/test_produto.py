ab30179962bdbc228cc803366c5fe376f9d8be3b
unknown <nicolas.raony06@gmail.com>
2026-01-08 20:10:48 -0300

    Add: teste para alterar_preco_produto gerente

--- tests/test_produto.py
+++ tests/test_produto.py
@@ -1,17 +1,26 @@
 # teste de implementacoes 
 # python -m tests.test_produto
 from src.farmacia.produto import Produto
+from src.farmacia.farmacia import Farmacia
+
+farmacia = Farmacia("Pague mais")
+
+farmacia.registrarGerente("Teste Gerente", '598.487.125-08', '02072004', 1899)
 
 produto_teste = Produto('Amitril',2.90,'Cimed')
-print(produto_teste.getId()) # teste de gerador de id
+print(produto_teste.getPreco()) # teste de gerador de id
+
+farmacia.gerente.alterar_preco_produto(produto_teste, 2.85)
+print(produto_teste.getPreco())
+print(produto_teste.getLogAlteracoes())
 
 produto_teste2 = Produto('Amitril',2.90,'Cimed')
 print(produto_teste2.getId()) # teste de gerador de id
 
-print(50*'-')
-print('PRODUTO-TESTE')
-print(f'ID: {produto_teste.getId()}')
-print(f'NOME: {produto_teste.nome}')
-print(f'PREÇO: {produto_teste.getPreco()}')
-print(f'FABRICANTE: {produto_teste.fabricante}')
-print(50*'-')
+# print(50*'-')
+# print('PRODUTO-TESTE')
+# print(f'ID: {produto_teste.getId()}')
+# print(f'NOME: {produto_teste.nome}')
+# print(f'PREÇO: {produto_teste.getPreco()}')
+# print(f'FABRICANTE: {produto_teste.fabricante}')
+# print(50*'-')
