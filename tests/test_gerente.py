c274fa8c5bd942240acdf82f1317fe9ffc933e6d
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-01-14 16:26:26 -0300

    Create test_gerente.py

---
+++ tests/test_gerente.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from src.farmacia.farmacia import Farmacia
+from src.core.atendente import Atendente
+from src.core.cliente import Cliente
+from src.farmacia.produto import Produto
+from decimal import Decimal
+from src.farmacia.estoque import Estoque
+estoque = Estoque()
+farmacia = Farmacia("Pague Menos")
+p1 = Produto('Dipirona-25mg-Comprimido',2.00,'Cimed')
+p2 = Produto('Dorflex-5mg-Comprimido',1.90,'Sanofi')
+g = farmacia._registrarGerente("Chico",'122.222.222-23','1-1-1998',100)
+fun1 = g.cadrastar_funcionario("Alvim","123.123.123-23",'1-1-1998',890)
+fun2 = g.cadrastar_funcionario("LULU","123.321.123-32",'9-2-1990',22)
+g.adicionar_produto_estoque(p1,12,estoque)
+g.adicionar_produto_estoque(p2,2,estoque)
+print(estoque.produto_disponibilidade(p2,11))
