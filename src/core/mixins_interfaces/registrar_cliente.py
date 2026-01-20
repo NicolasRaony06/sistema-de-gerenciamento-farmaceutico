dc4e574a9585075ca77b63be417e80008a1ded3e
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-01-20 15:50:12 -0300

    Delete registrar_cliente.py

--- src/core/mixins_interfaces/registrar_cliente.py
+++
@@ -1,3 +0,0 @@
-class RegistrarClienteMixin:
-    def registrar_cliente(self, farmacia, nome: str, cpf: str, data_nascimento = None):
-        farmacia._registrarCliente(self, nome, cpf, data_nascimento)
