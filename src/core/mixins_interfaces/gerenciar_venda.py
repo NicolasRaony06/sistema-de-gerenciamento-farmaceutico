6c720b9e087c7e7017dbfc5a505a5f83e615e7c3
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2026-01-24 23:28:24 -0300

    Update gerenciar_venda.py

--- src/core/mixins_interfaces/gerenciar_venda.py
+++ src/core/mixins_interfaces/gerenciar_venda.py
@@ -3,20 +3,16 @@ class GerenciarVendaMixin:
         '''Registra nova venda em farmacia. E adiciona Venda em lista de vendas do funcionario.'''
         return self.getFarmacia()._criarVenda(self)
     
-    def adicionar_produto_venda(self, produto, quantidade):
+    def adicionar_produto_venda(self, id, quantidade):
         '''Adiciona produto em venda, ultima realizada por funcionario, caso produto esteja disponivel no estoque.'''
         estoque = self.getFarmacia()._estoque
-        if estoque.produto_disponibilidade(produto, quantidade):
-            self.getVendasRealizadas()[-1].adicionarProduto(produto, quantidade)
-            estoque.remover_produto(self, produto.getId(), quantidade)
-            return True
-        raise ValueError("Produto indisponível no estoque")
+        return estoque.remover_produto(self,id,quantidade)
 
     def adicionar_cliente_venda(self, cliente):
         '''Adiciona cliente em ultima venda registrada pelo funcionario.'''
         self.getVendasRealizadas()[-1].adicionarCliente(cliente)
 
-    def finalizar_venda(self):
+    def finalizar_venda(self,venda):
         '''Finaliza ultima venda realizada pelo funcionario.'''
-        self.getVendasRealizadas()[-1].finalizarVenda()
+        return venda.finalizarVenda()
         
